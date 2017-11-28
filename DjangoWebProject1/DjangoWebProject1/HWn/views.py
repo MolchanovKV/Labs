941880ef2ddc980659b729375f49be738eb06e51
MolchanovKV <molchanov.94@mail.ru>
2017-11-28 22:54:28 +0400

    lab 2

--- DjangoWebProject1/DjangoWebProject1/HWn/views.py
+++ DjangoWebProject1/DjangoWebProject1/HWn/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 def home(request):
     return HttpResponse (render_to_string('index.html',{'content': 'Hello world'}))
+def username(request):
+    return HttpResponse (render_to_string('index.html',{'content': 'Hello %s!' %'Konstantin'}))
--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -5,10 +5,12 @@ Definition of urls for DjangoWebProject1.
 from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
+from HWn import views
 
 
 urlpatterns = patterns('',
     
-    url(r'^$', 'DjangoWebProject1.HWn.views.home', name='home'),
+    url(r'^$', views.home),
+    url(r'^usename/$', views.username),
     
 )
